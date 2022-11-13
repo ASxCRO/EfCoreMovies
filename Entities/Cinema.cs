229106f438b02b0ee54d7dfdd446deff72d201c5
asxcro <antonio.suups@gmail.com>
2022-11-13 19:26:51 +0100

    Add project files.

---
+++ Entities/Cinema.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+namespace EfCoreMovies.Entities
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Point Location { get; set; }
+
+        public CinemaOffer CinemaOffer { get; set; }
+        public List<CinemaHall> CinemaHalls{ get; set; }
+    }
+}
---
+++ Entities/CinemaHall.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EfCoreMovies.Entities
+{
+    public class CinemaHall
+    {
+        public int Id { get; set; }
+        public CinemaHallType CinemaHallType{ get; set; }
+        public decimal Cost { get; set; }
+        public int CinemaId { get; set; }
+        public Cinema Cinema { get; set; }
+        public HashSet<Movie> Movies { get; set; }
+
+    }
+}
